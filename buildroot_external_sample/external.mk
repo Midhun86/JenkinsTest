e7a196b74014b6207085a9c19dade1fda541b1df
RahulRaghukumar <rahul.raghukumar@trenser.com>
2025-10-24 16:56:07 +0530

    Updated external makefile.

--- buildroot_external_sample/external.mk
+++ buildroot_external_sample/external.mk
@@ -1,7 +1,3 @@
-<<<<<<< Updated upstream
-include $(sort $(wildcard $(BR2_EXTERNAL_BUILDROOT_EXTERNAL_SAMPLE_PATH)/package/*/*.mk))
-=======
 include $(sort $(wildcard $(BR2_EXTERNAL_BUILDROOT_EXTERNAL_SAMPLE_PATH)/package/*/*.mk))
 
 BR2_ROOTFS_OVERLAY += $(BR2_EXTERNAL_BUILDROOT_EXTERNAL_SAMPLE_PATH)/rootfs-overlay
->>>>>>> Stashed changes
