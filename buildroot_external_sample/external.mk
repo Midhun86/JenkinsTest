56f2ddfd501d9888b344181633d793453bf94046
Midhun Raj M <midhun.mukundan@trenser.com>
2025-11-03 15:04:38 +0530

    Added dht11 and i2c client drivers

--- buildroot_external_sample/external.mk
+++ buildroot_external_sample/external.mk
@@ -1,3 +1,4 @@
 include $(sort $(wildcard $(BR2_EXTERNAL_BUILDROOT_EXTERNAL_SAMPLE_PATH)/package/*/*.mk))
+include $(sort $(wildcard $(BR2_EXTERNAL_BUILDROOT_EXTERNAL_SAMPLE_PATH)/src/*/*/*.mk))
 
 BR2_ROOTFS_OVERLAY += $(BR2_EXTERNAL_BUILDROOT_EXTERNAL_SAMPLE_PATH)/rootfs-overlay
